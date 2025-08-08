8778eb08ae4ca268f097a3b521b767039f43b14f
DESKTOP-IHIDJ8G\PC <mucahitkalabalik@outlook.com>
2025-08-08 17:39:16 +0300

    excell export added.

--- Mobile/components/Home/HeaderSection.jsx
+++ Mobile/components/Home/HeaderSection.jsx
@@ -3,8 +3,47 @@ import logo from "@/assets/images/logo.png";
 import COLORS from "@/constants/colors";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useRouter } from "expo-router";
+import { useSelector } from "react-redux";
+
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
+import XLSX from "xlsx";
+
+async function exportToExcel(data) {
+  try {
+    // 1. JSON -> Excel dönüştür
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Transactions");
+
+    // 2. Excel dosyasını binary olarak yaz
+    const excelBinary = XLSX.write(workbook, {
+      type: "base64",
+      bookType: "xlsx",
+    });
+
+    // 3. Kaydedilecek dosya yolu
+    const filePath = FileSystem.documentDirectory + "transactions.xlsx";
+
+    // 4. Dosyayı yaz
+    await FileSystem.writeAsStringAsync(filePath, excelBinary, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    // 5. Paylaş
+    if (await Sharing.isAvailableAsync()) {
+      await Sharing.shareAsync(filePath);
+    } else {
+      alert("Paylaşım bu cihazda desteklenmiyor: " + filePath);
+    }
+  } catch (error) {
+    console.error("Excel export hatası:", error);
+  }
+}
 
 function HeaderSection({ user }) {
+  const { transactions } = useSelector((state) => state.transactions);
+
   const router = useRouter();
   return (
     <View style={styles.headerContainer}>
@@ -21,7 +60,7 @@ function HeaderSection({ user }) {
         <Text style={{ color: "white", marginLeft: "5" }}>Add</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        onPress={() => console.log("Button Pressed")}
+        onPress={() => exportToExcel(transactions)}
         style={styles.exportButton}
       >
         <AntDesign name="export" size={24} color={COLORS.primary} />
